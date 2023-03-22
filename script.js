da90ebc109d35d2ff554dc5002de8cf145a7449c
CollyLee <clh5160@gmail.com>
2023-03-22 15:21:30 -0400

    created randomizers to select one random unit from an array

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // user selects what arrays to work from (which leads to the selection of a final array)
-var question1 = confirm("Would you like to include uppercase?")
-var question2 = confirm("Would you like to include numbers?")
-var question3 = confirm("Would you like to include special characters?")
+// var question1 = confirm("Would you like to include uppercase?")
+// var question2 = confirm("Would you like to include numbers?")
+// var question3 = confirm("Would you like to include special characters?")
 
 // determines the length of the final password
-var question4 = prompt("How many characters would you like your password to be?/(max 258)")
+// var question4 = prompt("How many characters would you like your password to be?/(max 258)")
 
 // starter arrays, will be the building blocks of the final concat arrays
-var lowercase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var specialChar: ["!", "^", "@"]
-var number: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specialChar = ["!", "^", "@"]
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 // arrays used based on the user's answers to the questions
 var array01 = lowercase.concat(uppercase)
@@ -35,50 +35,46 @@ var array02 = lowercase.concat(specialChar)
 var array023 = lowercase.concat(specialChar, number)
 var array03 = lowercase.concat(number)
 
-function generatePassword: function () { 
-  
-var password = []
-
-// to pick random lowercase
-Math.floor(Math.random() * lowercase.length);
-
-// to pick random uppercase
-Math.floor(Math.random() * lowercase.length);
-
-// to pick random special chars
-Math.floor(Math.random() * specialChar.length);
-  
-// to pick random numbers
-Math.floor(Math.random() * number.length);
+// selects one random number from the necessary array
+var ranarray01 = Math.floor(Math.random() * array01.length);
+var ranarray012 = Math.floor(Math.random() * array012.length);
+var ranarray0123 = Math.floor(Math.random() * array0123.length);
+var ranarray02 = Math.floor(Math.random() * array02.length);
+var ranarray023 = Math.floor(Math.random() * array023.length);
+var ranarray03 = Math.floor(Math.random() * array03.length);
 
 
+
+
+function generatePassword() { 
+  var question1 = confirm("Would you like to include uppercase?")
+  var question2 = confirm("Would you like to include numbers?")
+  var question3 = confirm("Would you like to include special characters?")
+  var question4 = prompt("How many characters would you like your password to be?/(max 258)")
+}
 
 if (question1 && question2 && question3) { 
-var random = Math.floor(Math.random() * lowercase.length);
-password.concat(lowercase[random])
-
-} else if (question1 && question2) {
-  _____;
-
-} else if (question3) {
-  _____;
-
-} else if (question2 && question3) {
-  _____;
-
-} else if (question2) {
-  _____;
-
-} else if (question1 && question3) {
-  _____;
-
-} else {
-  _____;
-}
-
-}
+  var password = [];
+} 
 
 
 
 
-// 50 char, numbers, special characters
+// else if (question1 && question2) {
+//   var password = [];
+
+// } else if (question3) {
+//   var password = [];
+
+// } else if (question2 && question3) {
+//   var password = [];
+
+// } else if (question2) {
+//   var password = [];
+
+// } else if (question1 && question3) {
+//   var password = [];
+
+// } else {
+//   alert("You must choose at least one");
+// }
