ee7b6c68606e45804a843e93d3d188d3644152d6
CollyLee <clh5160@gmail.com>
2023-03-22 13:09:50 -0400

    created object and if blueprint

--- script.js
+++ script.js
@@ -13,16 +13,33 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var specialChar = ["!", "^", "@"]
-var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
 // First pop-up question
+var question1 = confirm("Would you like to include uppercase?")
+var question2 = confirm("Would you like to include numbers?")
+var question3 = confirm("Would you like to include special characters?")
 
-function generatePassword () {
-  (confirm("Would you like to include uppercase?"));
-  (confirm("Would you like to include special character?"));
+var password = {
+  lowercase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  uppercase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  specialChar: ["!", "^", "@"]
+  number: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+  generatePassword: function () {
 
-  return
+  }
+}
+
+if (question1 && question2 && question3) {
+  _____;
+} else if (question1 && question2) {
+  _____;
+} else if (question3) {
+  _____;
+} else if (question2 && question3) {
+  _____;
+} else if (question2) {
+  _____;
+} else if (question1 && question3) {
+  _____;
+} else {
+  _____;
 }
