9815e7f9e7908992c0a0e146ce92a2c2cb12bbe5
CollyLee <clh5160@gmail.com>
2023-03-23 10:21:33 -0400

    fixed function code, renamed vars

--- script.js
+++ script.js
@@ -5,75 +5,82 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
+generateBtn.addEventListener("click", generatePassword);
 
 
 // starter arrays, will be the building blocks of the final concat arrays
+var userArray = []
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var specialChar = ["!", "^", "@"]
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-// arrays used based on the user's answers to the questions
-var arrayLU = lowercase.concat(uppercase)
-var arrayLUS = lowercase.concat(uppercase, specialChar)
-var arrayLUSN = lowercase.concat(uppercase, specialChar, number)
-var arrayLS = lowercase.concat(specialChar)
-var arrayLSN = lowercase.concat(specialChar, number)
-var arrayLN = lowercase.concat(number)
-
-// coding to select ONE random item from the correct arrays based on user's answers
-var ranarrayLU = Math.floor(Math.random() * arrayLU.length);
-var ranarrayLUS = Math.floor(Math.random() * arrayLUS.length);
-var ranarrayLUSN = Math.floor(Math.random() * arrayLUSN.length);
-var ranarrayLS = Math.floor(Math.random() * arrayLS.length);
-var ranarrayLSN = Math.floor(Math.random() * arrayLSN.length);
-var ranarrayLN = Math.floor(Math.random() * arrayLN.length);
-
+password = []
 
 // function that is run when the Generate Password button is clicked
 function generatePassword() { 
-  var questionU = confirm("Would you like to include uppercase?")
-  var questionN = confirm("Would you like to include numbers?")
-  var questionS = confirm("Would you like to include special characters?")
-  var questionL = prompt("How many characters would you like your password to be?/(max 128)")
+  var questionLow = confirm("Would you like to include lowercase")
+  var questionUpp = confirm("Would you like to include uppercase?")
+  var questionNum = confirm("Would you like to include numbers?")
+  var questionSpe = confirm("Would you like to include special characters?")
+  var questionLen = prompt("How many characters would you like your password to be?/(max 128)")
 
-  // Pulls random items from the appropriate ranarray, looped for as many times as the user has selected character length
-  if (questionU && questionS && questionN) { 
-  for (let i = 0; i < arrayLUSN.length; i++) {
-  ranarrayLUSN;
-   }
-}
+  // concats the final array based on the answers to questionSpe
+ 
+  if (questionLow) {
+    userArray = userArray.concat(lowercase)
+  }
   
-  
+  if (questionUpp) {
+    userArray = userArray.concat(uppercase)
+  }
 
-alert(password) 
+  if (questionNum) {
+    userArray = userArray.concat(number)
+  }
+
+  if (questionSpe) {
+  userArray = userArray.concat(specialChar)
+  }
+
+  // need to add mandatory characters
+  for (let i=0; i < questionLen; i++) {
+    password.push(userArray[Math.floor(Math.random() * questionLen)])
+  }
+
+  password = password.join("")
+
+alert(password)
  }
 
 
-// else if (questionU && questionN) {
-//   var password = [];
 
-// } else if (questionS) {
-//   var password = [];
 
-// } else if (questionN && questionS) {
-//   var password = [];
 
-// } else if (questionN) {
-//   var password = [];
 
-// } else if (questionU && questionS) {
-//   var password = [];
 
-// } else {
-//   alert("You must choose at least one");
-// }
+
+
+
+
+ // // arrays used based on the user's answers to the questionSpe
+// var arrayLU = lowercase.concat(uppercase)
+// var arrayLUS = lowercase.concat(uppercase, specialChar)
+// var arrayLUSN = lowercase.concat(uppercase, specialChar, number)
+// var arrayLS = lowercase.concat(specialChar)
+// var arrayLSN = lowercase.concat(specialChar, number)
+// var arrayLN = lowercase.concat(number)
+
+// // coding to select ONE random item from the correct arrays based on user's answers
+// var ranarrayLU = Math.floor(Math.random() * arrayLU.length);
+// var ranarrayLUS = Math.floor(Math.random() * arrayLUS.length);
+// var ranarrayLUSN = Math.floor(Math.random() * arrayLUSN.length);
+// var ranarrayLS = Math.floor(Math.random() * arrayLS.length);
+// var ranarrayLSN = Math.floor(Math.random() * arrayLSN.length);
+// var ranarrayLN = Math.floor(Math.random() * arrayLN.length);
