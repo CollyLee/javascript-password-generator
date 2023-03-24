671435242b2e1f465bb7f04bdb467b6c1a6892dc
Colin Leidy <clh5160@gmail.com>
2023-03-24 14:44:55 -0400

    finalized JS coding

--- script.js
+++ script.js
@@ -24,6 +24,15 @@ password = []
 
 // function that is run when the Generate Password button is clicked
 function generatePassword() { 
+  // user sets the length of the final password
+  var questionLen = prompt("How many characters would you like your password to be?/(min 8, max 128)")
+
+  // gives user error message if their password isn't between 8 and 128 characters
+  if (questionLen < 8 || questionLen > 128) {
+    alert("Must be between 8 and 128 characters!")
+    return generatePassword()
+  }
+  
   var questionLow = confirm("Would you like to include lowercase")
   var questionUpp = confirm("Would you like to include uppercase?")
   var questionNum = confirm("Would you like to include numbers?")
@@ -34,16 +43,7 @@ function generatePassword() {
     alert("You have to choose at least one!")
     return generatePassword()
   }
-  
-  // user sets the length of the final password
-  var questionLen = prompt("How many characters would you like your password to be?/(min 8, max 128)")
-  
-  // gives user error message if their password isn't between 8 and 128 characters
-  if (questionLen < 8 || questionLen > 128) {
-    alert("Must be between 8 and 128 characters!")
-    return generatePassword()
-  }
-  
+
   // concats the final array based on the answers to questionSpe
   if (questionLow) {
     userArray = userArray.concat(lowercase);
@@ -65,11 +65,10 @@ function generatePassword() {
     password.push(specialChar[Math.floor(Math.random()*specialChar.length)])
   }
 
-  // reqChars = need a way to count how many questions were answered yes
-
-  // pushes one character to the final password, repeated based on how long the user wants final password to be
-  for (var i = 3; i < questionLen; i++) {
-    password.push(userArray[Math.floor(Math.random()*questionLen)])
+  // pushes one character to the final password, repeated based on how long the user wants final password to be minus whatever has already been pushed to the password
+  for (var i = password.length; i < questionLen; i++) {
+    password.push(userArray[Math.floor(Math.random()*userArray.length)])
+    console.log(password);
   }
 
   // joins the resulting array into one string
